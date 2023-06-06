c4187a0220bd841d93da99103ccd5c856e533a7f
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-06-06 14:01:05 -0700

    added files

---
+++ att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/att_doc_retriever_sample-5753b637c4164065.d
@@ -0,0 +1,5 @@
+/home/atello/Downloads/aws-nitro-enclaves-samples/att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/att_doc_retriever_sample-5753b637c4164065: src/main.rs
+
+/home/atello/Downloads/aws-nitro-enclaves-samples/att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/att_doc_retriever_sample-5753b637c4164065.d: src/main.rs
+
+src/main.rs:
---
+++ att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/nitro_enclave_attestation_document-0e7be84d898c8764.d
@@ -0,0 +1,7 @@
+/home/atello/Downloads/aws-nitro-enclaves-samples/att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/nitro_enclave_attestation_document-0e7be84d898c8764.rmeta: /home/atello/.cargo/git/checkouts/nitro-enclave-attestation-document-fcfae228514ff487/48c2e4c/src/lib.rs
+
+/home/atello/Downloads/aws-nitro-enclaves-samples/att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/libnitro_enclave_attestation_document-0e7be84d898c8764.rlib: /home/atello/.cargo/git/checkouts/nitro-enclave-attestation-document-fcfae228514ff487/48c2e4c/src/lib.rs
+
+/home/atello/Downloads/aws-nitro-enclaves-samples/att_doc_retriever_sample/rs/target/x86_64-unknown-linux-musl/release/deps/nitro_enclave_attestation_document-0e7be84d898c8764.d: /home/atello/.cargo/git/checkouts/nitro-enclave-attestation-document-fcfae228514ff487/48c2e4c/src/lib.rs
+
+/home/atello/.cargo/git/checkouts/nitro-enclave-attestation-document-fcfae228514ff487/48c2e4c/src/lib.rs:
