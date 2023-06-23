abcf2e97e6be0df7553f853db489b865e7bfa7f8
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-06-22 20:07:27 -0700

    debug

--- att_doc_retriever_sample/py/att_doc_retriever_sample.py
+++ att_doc_retriever_sample/py/att_doc_retriever_sample.py
@@ -112,12 +112,6 @@ def server_handler(args):
     server.send_data(out)
     print("Server Connection Closed")
 
-def decoder_handler(args):
-    server = vs.VsockListener()
-    server.bind(args.port)
-    server.recv_data()
-
-
 
 
 def main():
@@ -138,10 +132,6 @@ def main():
                                           help="Listen on a given port.")
     server_parser.add_argument("port", type=int, help="The local port to listen on.")
     server_parser.set_defaults(func=server_handler)
-
-    decoder_parser = subparsers.add_parser("decoder", description="Decoder Process", help="Test process")
-    decoder_parser.add_argument("port", type=int, help="The local port to listen on.")
-    server_parser.set_defaults(func=decoder_handler)
 
 
     if len(sys.argv) < 2:
