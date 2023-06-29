e7e8108b572d26a32ec9c06622554cde9ce9887d
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-06-29 01:22:28 -0700

    modified files

--- tls_connection_sample/py/tls_connection_sample.py
+++ tls_connection_sample/py/tls_connection_sample.py
@@ -93,7 +93,12 @@ class TLSClient:
 
     def retrieve_ca_certificate(self):
         with open(self.ca_certfile, 'wb') as ca_cert_file:
-            ca_cert_file.write(ssl.get_server_certificate((str(self.cid), str(self.port))).encode())
+            with socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM) as temp_sock:
+                temp_sock.connect((self.cid, self.port))
+                context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+                with context.wrap_socket(temp_sock, server_hostname=str(self.cid)) as ssl_temp_sock:
+                    peer_cert = ssl_temp_sock.getpeercert()
+                    ca_cert_file.write(ssl.DER_cert_to_PEM_cert(peer_cert))
 
     def connect(self):
         self.retrieve_ca_certificate()
