4068a8bff6c2d55cfe5f990f7a7b131d1e7c3402
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-14 07:52:15 +0200

    modified script

--- att_doc_retriever_sample/tls_script.sh
+++ att_doc_retriever_sample/tls_script.sh
@@ -7,7 +7,7 @@ docker build -t att-doc-retriever-sample -f Dockerfile --build-arg ARCH=$(uname 
 nitro-cli build-enclave --docker-uri att-doc-retriever-sample --output-file att_doc_retriever_sample.eif
 
 # Run enclave
-sudo nitro-cli run-enclave --cpu-count 2 --memory 1936 --enclave-cid 16 --eif-path att_doc_retriever_sample.eif --debug-mode
+sudo nitro-cli run-enclave --cpu-count 2 --memory 1940 --enclave-cid 16 --eif-path att_doc_retriever_sample.eif --debug-mode
 
 # Run Python script
 python3 py/att_doc_retriever_sample.py tls_client 16 5010
