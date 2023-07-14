dd636ac0563508d9a0a2088503d01ede71a28b28
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-14 13:19:55 +0200

    removed flag

--- tls_connection_sample/py/tls_connection_sample.py
+++ tls_connection_sample/py/tls_connection_sample.py
@@ -157,20 +157,21 @@ class TLSClient:
     def connect(self):
         # self.retrieve_ca_certificate()
         # self.add_ca_certificate_to_trust_store()
-
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        context.load_verify_locations(cafile=self.ca_certfile)
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
 
         self.client_sock = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
         server_address = (self.cid, self.port)
-        # self.client_sock.connect(server_address)
+        self.client_sock.connect(server_address)
 
         # context.load_verify_locations()
-
+        # context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+        # context.load_verify_locations(cafile=self.ca_certfile)
         # context.load_verify_locations("/etc/ssl/certs/ca-bundle.crt")
 
         ssl_client_sock = context.wrap_socket(self.client_sock, server_hostname=str(self.cid))
-        ssl_client_sock.connect(server_address)
+
         print("TLS Client connected")
 
         data = ssl_client_sock.recv(1024)
