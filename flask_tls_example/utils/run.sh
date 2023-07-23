d18ffb3e638e5a692fe1169f9ee894d8258d44c1
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-23 11:35:33 +0200

    added new container

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -5,7 +5,7 @@ ip link set dev lo up
 
 # Replace these values with the desired port numbers you want to use
 HOST_PORT=5000
-DOCKER_PORT=8080
+DOCKER_PORT=80
 
-# Route traffic from host port 5000 to Docker container port 8080 using vsock
-socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT &
+# Route traffic from host port 5000 to Docker container port 80 using vsock
+socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT
--- flask_tls_example/utils/setup.sh
+++ flask_tls_example/utils/setup.sh
@@ -2,7 +2,7 @@
 docker pull alpine/socat:latest
 
 # Replace these values with the desired port number you want to use
-PORT_NUMBER=8080
+PORT_NUMBER=5000
 
 # Run the Docker container with socat
-docker run -d -p $PORT_NUMBER:$PORT_NUMBER --name socat_container_$(date +%Y%m%d%H%M%S) alpine/socat tcp-listen:$PORT_NUMBER,fork,keepalive,reuseaddr vsock-connect:16:8080,keepalive
+docker run -d -p $PORT_NUMBER:$PORT_NUMBER --name socat_container_$(date +%Y%m%d%H%M%S) alpine/socat tcp-listen:$PORT_NUMBER,fork,keepalive,reuseaddr vsock-connect:16:80,keepalive
