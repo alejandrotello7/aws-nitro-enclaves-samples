e432eef70371b8d175dd747385b852e6e0c06cd8
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-23 23:35:52 +0200

    added new command

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 app.py
+
 
 echo "Flask app is ready! Starting socat..."
 
@@ -13,3 +13,5 @@ DOCKER_PORT=80
 
 # Route traffic from host port 5000 to Docker container port 80 using vsock
 socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT
+
+python3 app.py
