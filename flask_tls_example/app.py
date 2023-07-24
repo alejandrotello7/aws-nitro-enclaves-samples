00da22f64a5ce828bde9d1d58f041723b6e3220a
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-24 11:18:17 +0200

    Added new file

--- flask_tls_example/app.py
+++ flask_tls_example/app.py
@@ -5,19 +5,24 @@ import subprocess as sp
 import os
 
 app = Flask(__name__)
+attested_document_server = None
+
 
 @app.route('/')
 def index():
-    return "Hello Nitrogen! This is the default message."
+    return "Hello from the enclave! This is the default message."
+
 
 @app.route('/api/attestation')
 def attestation():
+    global attested_document_server
     current_dir = os.path.dirname(os.path.abspath(__file__))
     rs_binary = os.path.join(current_dir, 'att_doc_retriever_sample')
     proc = sp.Popen([rs_binary], stdout=sp.PIPE)
     out, err = proc.communicate()
     attested_document_server = json.loads(out)
     return attested_document_server
+
 
 @app.route('/api/execute', methods=['POST'])
 def upload_file():
@@ -42,10 +47,17 @@ def upload_file():
         return jsonify({"error": "No file received."}), 400
 
 
+@app.route('/api/attestion/public_key')
+def get_public_ket():
+    return attested_document_server['public_key']
+
+
 @app.route('/api/message2')
 def message2():
     return "This is message 2."
 
+
 if __name__ == '__main__':
     print('Starting flask app...')
+    attestation()
     app.run(host='0.0.0.0', port=80)
