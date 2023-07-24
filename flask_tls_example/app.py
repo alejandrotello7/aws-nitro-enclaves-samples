d861949ea20b473470869a08bf35973648e9465e
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-24 11:49:15 +0200

    Added new file

--- flask_tls_example/app.py
+++ flask_tls_example/app.py
@@ -3,10 +3,12 @@ import json
 from flask import Flask, request, jsonify
 import subprocess as sp
 import os
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 app = Flask(__name__)
 attested_document_server = None
-attested_document_valid_options = \
+ATTESTED_DOCUMENT_VALID_OPTIONS = \
     {'pcrs',
      'nonce',
      'module_id',
@@ -57,18 +59,53 @@ def upload_file():
 @app.route('/api/attestation/<arg>', methods=['GET'])
 def get_attested_arg(arg):
     global attested_document_server  # Access the global variable
-    global attested_document_valid_options  # Access valid options globally
+    global ATTESTED_DOCUMENT_VALID_OPTIONS  # Access valid options globally
 
     # Check if attestation has been performed
     if attested_document_server is not None:
         # Check if the provided argument is in the set of valid options
-        if arg in attested_document_valid_options:
+        if arg in ATTESTED_DOCUMENT_VALID_OPTIONS:
             return jsonify({arg: attested_document_server[arg]})
         else:
             return jsonify({
-                               "error": f"Invalid argument '{arg}'. Valid options are: {', '.join(attested_document_valid_options)}"}), 400
+                               "error": f"Invalid argument '{arg}'. Valid options are: {', '.join(ATTESTED_DOCUMENT_VALID_OPTIONS)}"}), 400
     else:
         return jsonify({"error": "Attestation not performed yet."}), 400
+
+
+@app.route('/api/encode', methods=['POST'])
+def encode_message():
+    # Get the message from the request
+    message = request.form.get('message')
+    if not message:
+        return jsonify({"error": "Message not provided."}), 400
+
+    # Retrieve the private key path from attested_document_server
+    private_key_path = attested_document_server['private_key_path']
+
+    try:
+        # Load the private key from the file
+        with open(private_key_path, 'rb') as key_file:
+            private_key = serialization.load_pem_private_key(
+                key_file.read(),
+                password=None
+            )
+
+        # Sign the message using the private key
+        encoded_message = private_key.sign(
+            message.encode('utf-8'),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+
+        # Return the encoded message as a response
+        return jsonify({"encoded_message": encoded_message.hex()}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"Error encoding message: {str(e)}"}), 500
 
 
 @app.route('/api/message2')
