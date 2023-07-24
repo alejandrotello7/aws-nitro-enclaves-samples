9684a1958f5d897f4fac965fd8f405ef40f68d2a
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-24 10:53:48 +0200

    Added new file

--- flask_tls_example/app.py
+++ flask_tls_example/app.py
@@ -1,27 +1,54 @@
 import json
 
-from flask import Flask
-import subprocess as sp
+from flask import Flask, request, jsonify
+import subprocess
 import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Hello Nitrogen! This is the default message."
 
+
 @app.route('/api/attestation')
-def message1():
+def attestation():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     rs_binary = os.path.join(current_dir, 'att_doc_retriever_sample')
-    proc = sp.Popen([rs_binary], stdout=sp.PIPE)
+    proc = subprocess.Popen([rs_binary], stdout=sp.PIPE)
     out, err = proc.communicate()
     attested_document_server = json.loads(out)
     return attested_document_server
 
+
+@app.route('/api/execute', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        # Save the uploaded file
+        file_path = os.path.join(os.getcwd(), 'uploaded_file.bin')
+        file.save(file_path)
+
+        # Make the file executable (optional, depending on your use case)
+        os.chmod(file_path, 0o755)
+
+        try:
+            # Execute the uploaded binary file
+            proc = subprocess.Popen([file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = proc.communicate()
+            return jsonify({"stdout": out.decode('utf-8'), "stderr": err.decode('utf-8')}), 200
+        except Exception as e:
+            return jsonify({"error": f"Error executing binary: {str(e)}"}), 500
+
+    else:
+        return jsonify({"error": "No file received."}), 400
+
+
 @app.route('/api/message2')
 def message2():
     return "This is message 2."
+
 
 if __name__ == '__main__':
     print('Starting flask app...')
