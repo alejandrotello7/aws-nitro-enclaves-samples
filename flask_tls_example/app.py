4c8fcdd4320dd8f53b4ed492416368b454e3d964
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-24 10:22:20 +0200

    added new call

--- flask_tls_example/app.py
+++ flask_tls_example/app.py
@@ -1,4 +1,7 @@
+import json
+
 from flask import Flask
+import subprocess as sp
 
 app = Flask(__name__)
 
@@ -6,9 +9,13 @@ app = Flask(__name__)
 def index():
     return "Hello Nitrogen! This is the default message."
 
-@app.route('/api/message1')
+@app.route('/api/attestation')
 def message1():
-    return "This is message 1."
+    rs_binary = 'att_doc_retriever_sample'
+    proc = sp.Popen([rs_binary], stdout=sp.PIPE)
+    out, err = proc.communicate()
+    attested_document_server = json.loads(out)
+    return attested_document_server
 
 @app.route('/api/message2')
 def message2():
