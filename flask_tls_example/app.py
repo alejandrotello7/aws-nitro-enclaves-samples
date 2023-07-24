08b77ce6440dd860cfe2fcb670732be97f6b7b91
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-24 10:30:14 +0200

    added new file

--- flask_tls_example/app.py
+++ flask_tls_example/app.py
@@ -2,6 +2,7 @@ import json
 
 from flask import Flask
 import subprocess as sp
+import os
 
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ def index():
 
 @app.route('/api/attestation')
 def message1():
-    rs_binary = 'att_doc_retriever_sample'
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    rs_binary = os.path.join(current_dir, 'att_doc_retriever_sample')
     proc = sp.Popen([rs_binary], stdout=sp.PIPE)
     out, err = proc.communicate()
     attested_document_server = json.loads(out)
