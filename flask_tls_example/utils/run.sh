bbb3f5e1446cdd7eb3ff8c5ac31be12a95856d01
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-25 14:19:01 +0200

    Added new file

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -6,15 +6,15 @@ ip link set dev lo up
 
 # Replace these values with the desired port numbers you want to use
 HOST_PORT=5000
-DOCKER_PORT=443
+DOCKER_PORT=8000
 
 
 # Route traffic from host port 5000 to Docker container port 8000 using vsock
 socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT &
 
-sleep 5
+sleep 20
 
-#gunicorn app:app --bind 0.0.0.0:8000 --workers 4
+gunicorn app:app --bind 0.0.0.0:8000 --workers 4
 
 # Run the Flask app using Gunicorn (you can replace 'app:app' with your app's entry point)
-python3 app.py
+#python3 app.py
