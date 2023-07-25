5c193a454ec0d86e6a28ba7be9ad820df9c84da6
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-25 13:52:40 +0200

    added new key

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -15,7 +15,6 @@ socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT 
 sleep 20
 
 #gunicorn app:app --bind 0.0.0.0:8000 --workers 4
-nginx -g "daemon off;"
 
 # Run the Flask app using Gunicorn (you can replace 'app:app' with your app's entry point)
 python3 app.py
