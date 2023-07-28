89f12963381fdfd053cba94b21c50f7addf0c5ff
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-28 13:02:39 +0200

    modified key

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -11,12 +11,11 @@ DOCKER_PORT=443
 # Route traffic from host port 5000 to Docker container port 443 using vsock
 socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT &
 
-python3 grpc_server.py
-sleep 10
+#python3 grpc_server.py
+#sleep 10
 
 # Start Gunicorn in the background
 gunicorn app:app --bind 0.0.0.0:8000 --workers 4 &
-#python3 app.py
 
 # Wait for a short period to allow Gunicorn to start fully (adjust the sleep duration as needed)
 sleep 10
