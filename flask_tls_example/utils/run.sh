1fe40d7de7946c770681342b17ce6f793c14e258
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-07-28 13:10:41 +0200

    modified key

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -20,9 +20,6 @@ gunicorn app:app --bind 0.0.0.0:8000 --workers 4 &
 # Wait for a short period to allow Gunicorn to start fully (adjust the sleep duration as needed)
 sleep 10
 
-python3 grpc_server.py
-sleep 10
-
 python3 grpc_server.py > grpc_server.log 2>&1 &
 sleep 10
 
