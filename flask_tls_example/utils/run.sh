284522ba914f26d16d03cc07a36ac626dab46b74
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-08-17 15:07:34 +0200

    modified setup|

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -7,8 +7,14 @@ ip link set dev lo up
 HOST_PORT=5000
 DOCKER_PORT=443
 
+REGULAR_PORT=80
+
+
 # Route traffic from host port 5000 to Docker container port 443 using vsock
 socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT &
+
+socat vsock-listen:REGULAR_PORT,reuseaddr,fork tcp-connect:127.0.0.1:REGULAR_PORT &
+
 
 #python3 grpc_server.py
 #sleep 10
