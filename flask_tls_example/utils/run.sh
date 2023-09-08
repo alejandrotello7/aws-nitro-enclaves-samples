d4b6dcc0d3c64e7e04d4e065bc3f895f25cbbd3d
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-09-08 06:44:36 +0200

    added library

--- flask_tls_example/utils/run.sh
+++ flask_tls_example/utils/run.sh
@@ -8,8 +8,8 @@ HOST_PORT=5000
 DOCKER_PORT=443
 REGULAR_PORT=80
 
-ls
-LD_LIBRARY_PATH=. LD_PRELOAD=syscall_intercept/examples/example_open.so ./syscall_intercept/examples/my_program
+
+LD_LIBRARY_PATH=syscall_intercept/ LD_PRELOAD=syscall_intercept/examples/example_open.so ./syscall_intercept/examples/my_program
 
 # Route traffic from host port 5000/80 to Docker container port 443/80 using vsock
 socat vsock-listen:$HOST_PORT,reuseaddr,fork tcp-connect:127.0.0.1:$DOCKER_PORT &
