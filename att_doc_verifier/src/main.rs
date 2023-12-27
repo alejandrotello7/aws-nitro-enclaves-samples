4e6c570983c5ddbe0f44cb63608258e9043078e4
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-27 15:35:07 -0800

    Added new file

--- att_doc_verifier/src/main.rs
+++ att_doc_verifier/src/main.rs
@@ -74,8 +74,8 @@ fn main() {
     let binding = read(file_path).unwrap();
     let cert = binding.as_slice();
     let nonce_value = "nonce";
-    let nonce_length: usize = nonce_value.len();
-    const BASE_SIZE: usize = 4893;
+    let _nonce_length: usize = nonce_value.len();
+    const BASE_SIZE: usize = 4894;
     // const BASE_SIZE: usize = 4887;
     // const MAX_SIZE_NONCE: usize = 100;
     // const ATT_MAX_DOCUMENT_SIZE: usize = BASE_SIZE + MAX_SIZE_NONCE;
