b4c684ff72bf1ba16ddfc26065febfee6ec206a2
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-27 17:57:28 -0800

    added new server

--- flask_tls_example/utils/setup.sh
+++ flask_tls_example/utils/setup.sh
@@ -12,4 +12,3 @@ docker run -d -p $PORT_NUMBER:$PORT_NUMBER --name socat_$PORT_NUMBER alpine/soca
 
 docker run -d -p $REGULAR_PORT:$REGULAR_PORT --name socat_$REGULAR_PORT alpine/socat tcp-listen:$REGULAR_PORT,fork,keepalive,reuseaddr vsock-connect:16:80,keepalive
 
-docker run -d -p 6000:6000 --name socat_6000 alpine/socat tcp-listen:6000,fork,keepalive,reuseaddr vsock-connect:16:6000,keepalive
