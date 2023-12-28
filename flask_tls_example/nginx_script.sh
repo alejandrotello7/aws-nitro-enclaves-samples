242053cfbab042cc8c72ba00325eadbebc7ebe75
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-27 16:17:58 -0800

    added new files

--- flask_tls_example/nginx_script.sh
+++ flask_tls_example/nginx_script.sh
@@ -7,7 +7,7 @@ docker build -t nginx-sample -f Dockerfile .
 nitro-cli build-enclave --docker-uri nginx-sample --output-file nginx-sample.eif
 
 # Run enclave
-sudo nitro-cli run-enclave --cpu-count 2 --memory 2524 --enclave-cid 16 --eif-path nginx-sample.eif
+sudo nitro-cli run-enclave --cpu-count 2 --memory 2524 --enclave-cid 16 --eif-path nginx-sample.eif --debug-mode
 
 #Make setup executable
 chmod +x utils/setup.sh
