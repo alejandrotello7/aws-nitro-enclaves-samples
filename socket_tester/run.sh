59846981fc2715b2b179d524c9891cc8c59a696f
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-29 19:25:26 -0800

    modified script

--- socket_tester/run.sh
+++ socket_tester/run.sh
@@ -3,5 +3,5 @@
 ip addr add 127.0.0.1/32 dev lo
 ip link set dev lo up
 
-socat tcp-listen:7000,reuseaddr,fork vsock-connect:VMADDR_CID_HOST:6000 &
+socat tcp-listen:7000,reuseaddr,fork vsock-connect:16:6000 &
 python3 client.py
