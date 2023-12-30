afede4bac6500a7f57755d50b96dde2e79f6ca77
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-29 19:17:38 -0800

    added new tester

--- socket_tester/starter_script.sh
+++ socket_tester/starter_script.sh
@@ -10,7 +10,7 @@ nitro-cli build-enclave --docker-uri socket-sample --output-file socket-sample.e
 sudo nitro-cli run-enclave --cpu-count 2 --memory 2524 --enclave-cid 16 --eif-path socket-sample.eif --debug-mode
 
 #Make setup executable
-chmod +x utils/socat_ec2_host.sh
+chmod +x socat_ec2_host.sh
 
 #Run the setup.sh script
 ./socat_ec2_host.sh
