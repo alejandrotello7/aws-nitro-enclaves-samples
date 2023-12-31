e68550490856e054ac68db62b13d88959cb7e8ea
Alejandro Tello <alejandro.tello.f@gmail.com>
2023-12-30 16:51:02 -0800

    added new keys|

--- syscall_interceptor/server.py
+++ syscall_interceptor/server.py
@@ -13,7 +13,7 @@ def run_server(port=6000):
                 sent_message = b"Hello from Server"
                 print(f"Received:{data}")
                 if not data:
-                    break
+                    print("not yet")
                 conn.sendall(sent_message)  # Echoes back received data
 
 if __name__ == "__main__":
