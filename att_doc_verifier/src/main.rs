66dd647b6e4fe5d6640cdc6d9ef4e6772aca570e
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-03 01:43:06 -0800

    modified verifier

--- att_doc_verifier/src/main.rs
+++ att_doc_verifier/src/main.rs
@@ -85,7 +85,7 @@ fn main() {
     let output = Command::new("curl")
         .arg("-X")
         .arg("GET")
-        .arg(format!("https://ec2-3-71-30-100.eu-central-1.compute.amazonaws.com:5000/api/attestation_retriever/{}",nonce_value))
+        .arg(format!("https://ec2-18-153-51-27.eu-central-1.compute.amazonaws.com:5000/api/attestation_retriever/{}",nonce_value))
         .arg("--header")
         .arg("Content-Type: text/html")
         .arg("--data")
