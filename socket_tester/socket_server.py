ef5f8919554321fbb9d30105ea513badd0033f33
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-02 17:03:30 -0800

    added new tls

--- socket_tester/socket_server.py
+++ socket_tester/socket_server.py
@@ -1,45 +1,35 @@
 import json
 import os
 import socket
+import ssl
 import threading
 import struct
 
-# Server configuration
-HOST = '0.0.0.0'
+HOST = '0.0.0.0'  # Use the appropriate host
 PORT = 50051
-file_descriptor = None
-file_object = None
 
-def handle_client(client_socket):
-    while True:  # Keep the connection open to handle multiple requests
-        buffer = b""  # Reset buffer for each request
+def handle_client(ssl_sock):
+    while True:
+        buffer = b""
         while True:
-            part = client_socket.recv(1024)
+            part = ssl_sock.recv(1024)
             buffer += part
             if not part or b'\n' in part:
-                break  # Exit loop if no more data or newline is found
+                break
         if not buffer:
-            break  # Exit the outer loop if no data is received (client closed connection)
+            break
 
         try:
-            # Decode buffer up to the first newline character
             data, _ = buffer.split(b'\n', 1)
             event_data = json.loads(data.decode('utf-8'))
 
-            # Process the JSON data
             response_int = process_json_data(event_data)
 
-            # Send the response back to the client
             response_int_network_order = socket.htonl(response_int)
             response_data = struct.pack('I', response_int_network_order)
-            client_socket.sendall(response_data)
-
-
+            ssl_sock.sendall(response_data)
         except json.JSONDecodeError as e:
             print(f"JSON decode error: {e}")
-            # Optionally send an error response back to the client
-
-    # client_socket.close()  # Close the connection when done
 
 def process_json_data(event_data):
     global file_object
@@ -49,11 +39,9 @@ def process_json_data(event_data):
         filename = event_data["filename"]
         print(f"Received message from BPF program: {filename}")
 
-        # Close the previously opened file
         if file_object:
             file_object.close()
 
-        # Open the new file
         file_object = open(filename, 'w')
         response_int = file_object.fileno()
         print(f'File descriptor returned: {response_int}')
@@ -65,7 +53,7 @@ def process_json_data(event_data):
             data = event_data["data"]
             print(f"Data received: {data}")
             bytes_written = os.write(file_descriptor, data.encode('utf-8'))
-            response_int = bytes_written;
+            response_int = bytes_written
             print(f"Response: {response_int}")
 
     elif event_data["operation"] == 3:
@@ -76,11 +64,14 @@ def process_json_data(event_data):
 
     elif event_data["operation"] == 4:
         file_descriptor = event_data["file_descriptor"]
-
+        # Add logic for read operation
 
     return response_int
 
 def start_server():
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_verify_locations('/home/ec2-user/dev/aws-nitro-enclaves-samples/flask_tls_example/utils/enclaves.pem')  # Path to CA certificate
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(5)
@@ -88,7 +79,8 @@ def start_server():
 
     while True:
         client_socket, addr = server_socket.accept()
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+        ssl_sock = context.wrap_socket(client_socket, server_side=True)
+        client_handler = threading.Thread(target=handle_client, args=(ssl_sock,))
         client_handler.start()
 
 if __name__ == "__main__":
