df6af9b7c63c9da0dc6eda499e5cdb708e263db4
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-04 01:56:10 -0800

    Added bdebugi message

--- enclave_runner/verifier_script.sh
+++ enclave_runner/verifier_script.sh
@@ -7,6 +7,8 @@ echo "$python3_output"
 
 # Extract and save pcr0 and enclave_id
 pcr0=$(echo "$python3_output" | grep "PCR0" | cut -d' ' -f2)
+pcr1=$(echo "$python3_output" | grep "PCR1" | cut -d' ' -f2)
+pcr2=$(echo "$python3_output" | grep "PCR2" | cut -d' ' -f2)
 enclave_id=$(echo "$python3_output" | grep "EnclaveID" | cut -d' ' -f2)
 
 # Navigate to Rust program directory and run it (A delayed of 10s is used to ensure enclave is up)
@@ -17,17 +19,20 @@ echo "$cargo_output"
 
 # Use jq to parse the JSON output and extract PCR0 and Module ID
 rust_pcr0=$(echo "$cargo_output" | jq -r '.pcrs.PCR0')
+rust_pcr1=$(echo "$cargo_output" | jq -r '.pcrs.PCR1')
+rust_pcr2=$(echo "$cargo_output" | jq -r '.pcrs.PCR2')
 rust_module_id=$(echo "$cargo_output" | jq -r '.module_id')
 
 
 # Add your logic for comparing with stored pcr0 and module_id
 rust_pcr0_lower=$(echo "$rust_pcr0" | tr '[:upper:]' '[:lower:]')
+rust_pcr1_lower=$(echo "$rust_pcr1" | tr '[:upper:]' '[:lower:]')
 # Print the extracted values
 echo "Extracted PCR0: $rust_pcr0_lower"
 echo "Extracted Module ID: $rust_module_id"
 
 
-if [[ "$pcr0" == "$rust_pcr0_lower" && "$enclave_id" == "$rust_module_id" ]]; then
+if [[ "$pcr0" == "$rust_pcr0_lower" && "$pcr1" == "$rust_pcr1_lower" ]]; then
     echo "Success: Verification passed."
 else
     echo "Failed: Verification failed."
