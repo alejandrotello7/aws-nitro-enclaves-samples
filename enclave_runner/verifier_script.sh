5cc74856c8ffa5d89631dce272e4d17f59d7d6c0
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-03 18:14:36 -0800

    Added verifier script

---
+++ enclave_runner/verifier_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Navigate to Python script directory and execute it
+cd /home/ec2-user/dev/aws-nitro-enclaves-samples/enclave_runner/
+python3_output=$(python3 spawner_client.py)
+echo "$python3_output"
+
+# Extract and save pcr0 and enclave_id
+pcr0=$(echo "$python3_output" | grep "PCR0" | cut -d' ' -f2)
+enclave_id=$(echo "$python3_output" | grep "EnclaveID" | cut -d' ' -f2)
+
+# Navigate to Rust program directory and run it
+cd /home/ec2-user/dev/aws-nitro-enclaves-samples/att_doc_verifier
+cargo_output=$(cargo run)
+echo "$cargo_output"
+
+# Extract pcr0 and module_id from Rust program output
+rust_pcr0=$(echo "$cargo_output" | grep "\"PCR0\"" | cut -d':' -f2 | tr -d ' ",')
+rust_module_id=$(echo "$cargo_output" | grep "module_id" | cut -d':' -f2 | tr -d ' ",')
+
+# Compare and output results
+if [[ "$pcr0" == "$rust_pcr0" && "$enclave_id" == "$rust_module_id" ]]; then
+    echo "Success: Verification passed"
+else
+    echo "Failed: Verification failed due to mismatch"
+fi
