48e8bc74bf7170c2c86e81aba9094987b05ff121
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-03 16:59:22 -0800

    added new api

--- enclave_parent_dir/api_server_spawner.py
+++ enclave_parent_dir/api_server_spawner.py
@@ -1,17 +1,25 @@
-# app.py
 from flask import Flask, jsonify
 import subprocess
+import json
 
 app = Flask(__name__)
+
 
 @app.route('/start_enclave', methods=['GET'])
 def start_enclave():
     try:
-        # Replace 'your_script.sh' with your actual script name
-        result = subprocess.check_output(['./your_script.sh'], shell=True)
-        return jsonify({"message": "Enclave started", "data": result.decode()}), 200
+        # Running the script and capturing its output
+        script_output = subprocess.check_output(['./your_script.sh'], shell=True).decode()
+
+        # Parse the output to extract EnclaveID and PCR0
+        output_lines = script_output.split('\n')
+        enclave_id = next(line.split(': ')[1] for line in output_lines if "EnclaveID" in line)
+        pcr0 = next(line.split(': ')[1] for line in output_lines if "PCR0" in line)
+
+        return jsonify({"EnclaveID": enclave_id, "PCR0": pcr0}), 200
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
