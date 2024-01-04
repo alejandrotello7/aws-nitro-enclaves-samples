57cf2bb2c434568ce0debc3f597bd4829850b501
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-04 01:28:16 -0800

    Added bdebugi message

--- enclave_runner/verifier_script.sh
+++ enclave_runner/verifier_script.sh
@@ -15,16 +15,20 @@ cd /home/ec2-user/dev/aws-nitro-enclaves-samples/att_doc_verifier
 cargo_output=$(cargo run)
 echo "$cargo_output"
 
-# Extract pcr0 and module_id from Rust program output
-rust_pcr0=$(echo "$cargo_output" | grep "\"PCR0\"" | cut -d':' -f2 | tr -d ' ",')
-rust_module_id=$(echo "$cargo_output" | grep "module_id" | cut -d':' -f2 | tr -d ' ",')
+# Extract pcr0 and module_id from the Rust program output
+rust_pcr0=$(echo "$cargo_output" | awk -F'[:,]' '/"PCR0"/ {gsub(/[^0-9a-fA-F]/, "", $2); print $2}')
+rust_module_id=$(echo "$cargo_output" | awk -F'[:,]' '/"module_id"/ {gsub(/[^0-9a-zA-Z-]/, "", $2); print $2}')
 
-# Compare and output results
-if [[ "$enclave_id" == "$rust_module_id" ]]; then
-    echo "Success: Verification passed"
+# Print the extracted values
+echo "Extracted PCR0: $rust_pcr0"
+echo "Extracted Module ID: $rust_module_id"
+
+# Add your logic for comparing with stored pcr0 and module_id
+stored_pcr0="your_stored_pcr0_value"
+stored_module_id="your_stored_module_id_value"
+
+if [[ "$rust_module_id" == "$stored_module_id" ]]; then
+    echo "Success: Verification passed."
 else
-    echo "Failed: Verification failed due to mismatch"
-    echo $enclave_id
-    echo rust_pcr0
-
+    echo "Failed: Verification failed."
 fi
