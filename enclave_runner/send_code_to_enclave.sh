be5f88439879b46274fa56012d5cc851bfec4cb3
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-01-12 01:28:58 +0100

    added file

--- enclave_runner/send_code_to_enclave.sh
+++ enclave_runner/send_code_to_enclave.sh
@@ -3,6 +3,6 @@
 cd /home/ec2-user/dev/aws-nitro-enclaves-samples/enclave_runner/socket_tester
 python3 socket_server.py &
 sleep 5
-curl -X POST -F "file=@/home/ec2-user/dev/aws-nitro-enclaves-samples/enclave_runner/tester_enclave_runner"  https://ec2-3-69-47-66.eu-central-1.compute.amazonaws.com:5000/api/execute_interceptor
+curl -X POST -F "file=@/home/ec2-user/dev/aws-nitro-enclaves-samples/enclave_runner/tester_enclave_runner_READONLY"  https://ec2-3-69-47-66.eu-central-1.compute.amazonaws.com:5000/api/execute_interceptor
 
 
