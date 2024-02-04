e20b28622c2d81a0cd23f703cea160752125d4f1
Alejandro Tello <alejandro.tello.f@gmail.com>
2024-02-04 13:17:47 +0100

    added new test

--- enclave_runner/docker_send_code_to_enclave.sh
+++ enclave_runner/docker_send_code_to_enclave.sh
@@ -9,4 +9,4 @@ sleep 5
 
 # Replace the file path with the correct path in the container
 # Also, update the URL if needed, especially if it refers to a service outside the container
-curl -X POST -F "file=@/usr/src/app/enclave_runner/tester_enclave_runner" https://ec2-3-70-8-55.eu-central-1.compute.amazonaws.com:5000/api/execute_interceptor
+curl -X POST -F "file=@/usr/src/app/enclave_runner/tester_enclave_runner" https://ec2-3-77-41-49.eu-central-1.compute.amazonaws.com:5000/api/execute_interceptor
